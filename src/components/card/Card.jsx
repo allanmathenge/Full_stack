a74cb96389bf18d538a4db145d50ba47e3bc72d1
allan <allanmathenge2@gmail.com>
2023-10-26 02:13:14 +0300

    First commit

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -10,20 +10,20 @@ const Card = () => {
     return (
         <div className={styles.container}>
             <div className={styles.imageContainer}>
-                <Image src="/p1.jpeg" alt="" fill />
+                <Image src="/p1.jpeg" alt="" fill className={styles.image} />
             </div>
             <div className={styles.textContainer}>
                 <div className={styles.detail}>
-                    <span className={styles.date}>{today}</span>
+                    <span className={styles.date}>{today} - </span>
                     <span className={styles.category}>CULTURE</span>
                 </div>
                 <Link href="/">
                     <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     </h1>
                 </Link>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quas eveniet debitis. Corrupti quis recusandae explicabo asperiores fugiat doloremque ducimus iusto, tempore maiores excepturi deserunt ipsum non, molestias reiciendis temporibus.
+                <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quas eveniet debitis. Corrupti quis recusandae explicabo asperiores fugiat doloremque ducimus iusto, tempore maiores excepturi deserunt ipsum non, molestias reiciendis temporibus.
                 </p>
-                <Link href="/">Read More</Link>
+                <Link href="/" className={styles.link}>Read More</Link>
             </div>
         </div>
     )
