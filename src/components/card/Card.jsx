49ff7970fe125f550dcd333a6e59a73a763ae087
allan <allanmathenge2@gmail.com>
2023-11-02 16:54:26 +0300

    commit

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,28 +1,26 @@
 import Link from "next/link"
 import styles from "./card.module.css"
 import Image from "next/image"
-import { getCurrentDate } from "../dates/Dates"
 
 const Card = ({ key, item }) => {
 
-    const today = getCurrentDate()
-
     return (
         <div className={styles.container} key={key}>
-            <div className={styles.imageContainer}>
-                <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-            </div>
+            {item.img && (
+                <div className={styles.imageContainer}>
+                    <Image src={item.img} alt="" fill className={styles.image} />
+                </div>
+            )}
             <div className={styles.textContainer}>
                 <div className={styles.detail}>
                     <span className={styles.date}>{item.createdAt.substring(0, 10)} - </span>
                     <span className={styles.category}>{item.catSlug}</span>
                 </div>
-                <Link href="/">
+                <Link href={`/posts/${item.slug}`}>
                     <h1>{item.title}</h1>
                 </Link>
-                <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quas eveniet debitis. Corrupti quis recusandae explicabo asperiores fugiat doloremque ducimus iusto, tempore maiores excepturi deserunt ipsum non, molestias reiciendis temporibus.
-                </p>
-                <Link href="/" className={styles.link}>Read More</Link>
+                <p className={styles.desc}>{item.desc.substring(0, 60)}</p>
+                <Link href={`/posts/${item.slug}`} className={styles.link}>Read More</Link>
             </div>
         </div>
     )
