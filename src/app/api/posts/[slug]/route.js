c564baf9a1ee5ffd6c5a3040b7856a9e4f2e7421
allan <allanmathenge2@gmail.com>
2023-11-04 14:43:18 +0300

    last commit

--- src/app/api/posts/[slug]/route.js
+++ src/app/api/posts/[slug]/route.js
@@ -7,8 +7,9 @@ export const GET = async (req, { params }) => {
 
     const { slug } = params;
 
-    const post = await prisma.post.findUnique({
+    const post = await prisma.post.update({
         where: { slug },
+        data: { views: { increment: 1 } },
         include: { user: true }
     });
 
